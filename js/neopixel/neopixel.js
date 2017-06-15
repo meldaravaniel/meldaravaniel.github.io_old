e680f8aaf6bad6a1ad4afe0e51a304c88e378c92
meldaravaniel <meldaravaniel@gmail.com>
2017-06-15 15:50:50 -0700

    cleaning up the rest

--- js/neopixel/neopixel.js
+++
@@ -1,9 +0,0 @@
-function Neopixel(){
-};
-
-Neopixel.prototype.getHexValue = function(decimal){
-	if(decimal < 0 || decimal > 255){
-		return "Invalid number.";
-	}
-	return decimal.toString(16);
-};
