b91590a88096b7196deb276a746c4e4e3e94f5f1
Amy Gleixner <meldaravaniel@gmail.com>
2017-07-07 23:52:00 -0700

    Adding a knit code module to house my soon to come pattern generator.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { KnitCodeModule } from './knit-code/knit-code.module';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
 
 @NgModule({
@@ -12,7 +13,8 @@ import { JumbotronComponent } from './jumbotron/jumbotron.component';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    KnitCodeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
